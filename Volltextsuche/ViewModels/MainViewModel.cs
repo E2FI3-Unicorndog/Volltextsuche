98f40927ad273922438f530f42a8098e27fc4808
schweitzer <lule.schweitzer@its-stuttgart.de>
2021-04-20 09:56:05 +0200

    Projektvorbereitung Volltextsuche

---
+++ Volltextsuche/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using CommandHelper;
+
+namespace Volltextsuche.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Variables
+
+        #endregion
+
+        #region Main
+
+        public MainViewModel(){}
+
+        #endregion
+
+        #region Properties
+
+
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region EventHandler
+
+        private void NotifyOnPropertyChanged(string propName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+    }
+}
