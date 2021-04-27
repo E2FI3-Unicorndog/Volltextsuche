7b6eafed6a68f7682184828e512c19e1b88bc739
schweitzer <lule.schweitzer@its-stuttgart.de>
2021-04-27 15:18:58 +0200

    Farbschema GUI + Splash Screen

--- Volltextsuche/obj/Debug/Volltextsuche_Content.g.i.cs
+++ Volltextsuche/obj/Debug/Volltextsuche_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("unicorndog-volltextsuche.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/images/unicorndog-volltextsuche.jpg")]
 
 
